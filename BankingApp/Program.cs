5a89be6fbd187c3c30813740c4fdeefc80dd6649
mark-stewart-117 <stewart-m22@ulster.ac.uk>
2025-09-29 12:50:30 +0100

    Created BankingApp and added menu & If else statements

--- BankingApp/Program.cs
+++ BankingApp/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Week 1 BankingApp
+ * Author: Mark Stewart
+ * Version 1
+ */
+void BuildBankingApp()
+{
+    //Initalise variable
+    //Note that an amount is not assigned to the variable
+    int choice;
+
+    //Display main menu screen
+    Console.WriteLine("Please choose an option");
+    Console.WriteLine("=======================");
+    Console.WriteLine("1 - Deposit or withdraw funds");
+    Console.WriteLine("2 - View current account information");
+    Console.WriteLine("Please choose an option (1-2)");
+
+    //Read user input
+    //Convert string to an integer
+    //using explicit typecasting
+
+    choice = Convert.ToInt32(Console.ReadLine());
+
+    //The || in the loop represents OR
+    //which means that a menu choice is 1 OR 2
+    //is accepted for this logical operation
+    if (choice == 1 || choice == 2)
+    {
+        //Valid choice menu
+        if (choice == 1)
+        {
+            //Deposit or withdraw funds
+            Console.WriteLine("You have chosen to deposit or withdraw funds");
+        }
+        else
+        {
+            //View current account information
+            Console.WriteLine("You have chosen to view current account information");
+        }
+    }
+    else
+    {
+        //Invalid menu choice
+        Console.WriteLine("Invalid choice, please choose 1 or 2");
+    }
+}
